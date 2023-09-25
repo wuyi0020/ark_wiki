e603da46be9856ec7918cbd2ed706588fe26fa1b
wuyi0020 <wuyi0020@gmail.com>
2023-09-25 21:44:25 +0800

    Vcg: Update live demo

--- vue.config.js
+++
@@ -1,8 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV ==='production'
-  ? '/ark_wiki/'
-  : '/',
-  transpileDependencies: true,
-  lintOnSave: false
-})
